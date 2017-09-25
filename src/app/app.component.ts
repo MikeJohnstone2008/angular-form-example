46603c9854dd1ae3ae2894091b85e06d7768a249
MikeJohnstone <michael.johnstone@libertymutual.com>
2017-09-25 13:52:12 -0700

    final withModel data validations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,5 +21,56 @@ export class AppComponent {
     console.log(data.value)
   }
 
+  ngAfterViewChecked() {
+    this.formChanged();
+  }
+
+  formChanged() {
+    //if the form didn't change then do nothing
+    if (this.currentForm === this.sampleForm) { return; }
+    //set the form to the current form for comparison
+    this.sampleForm = this.currentForm;
+    //subscribe to form changes and send the changes to the onValueChanged method
+    this.sampleForm.valueChanges
+      .subscribe(data => this.onValueChanged(data));
+  }
+
+  onValueChanged(data?: any) {
+    let form = this.sampleForm.form;
+
+    for (const field in this.formErrors) {
+      // clear previous error message (if any)
+      this.formErrors[field] = '';
+      const control = form.get(field);
+
+      if (control && control.dirty && !control.valid) {
+        const messages = this.validationMessages[field];
+        for (const key in control.errors) {
+          this.formErrors[field] += messages[key] + ' ';
+        }
+      }
+    }
+  }
+
+  //start out the errors as an emtpy string
+  formErrors = {
+    'first_name': '',
+    'last_name': ''
+  };
+ 
+validationMessages = {
+  'first_name': {
+    'required':      'First name is required.',
+    'minlength':     'Name must be at least 2 characters long.'
+  },
+  'last_name': {
+    'minlength':     'Name must be at least 2 characters long.'
+  }
+};
+
+
+
+
+
 
 }
